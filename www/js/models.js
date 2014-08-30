e7a6d44967c1fa5206fb57cd646ffc272cb875c5
arnef <git@arnefeil.de>
2014-08-30 22:32:01 +0200

    modelle angepasst

--- www/js/models.js
+++ www/js/models.js
@@ -4,28 +4,38 @@
  * and open the template in the editor.
  */
 
-function Action () {
+function Action() {
     this.name;
     this.tooltip;
     this.counting;
-    
+
     this.increateCounting = function() {
         this.counting++;
     };
-    
+
     this.hasTooltip = function() {
         return this.tooltip !== "";
     };
 }
 
-function Rule (withPlayer) {
+function Rule(withPlayer) {
+    this.name;
+    this.tooltip;
+    this.counting;
     this.user = "";
     this.withPlayer = withPlayer;
     this.active = false;
     this.visible = false;
+    this.increateCounting = function() {
+        this.counting++;
+    };
+
+    this.hasTooltip = function() {
+        return this.tooltip !== "";
+    };
 }
 
-function Game (users, actions70, actions20, actions10) {
+function Game(users, actions70, actions20, actions10) {
     this.users = users;
     this.actions70 = actions70;
     this.actions20 = actions20;
@@ -33,12 +43,12 @@ function Game (users, actions70, actions20, actions10) {
     this.activeRules;
     this.currentPlayer = -1;
     this.currentAction = null;
-    
-    this.next = function () {
-        var random  = Math.floor(Math.random() * 99);
+
+    this.next = function() {
+        var random = Math.floor(Math.random() * 99);
         var next = 0;
         if (random < 10) {
-            next = Math.floor(Math.random() * (this.actions10.length-1));
+            next = Math.floor(Math.random() * (this.actions10.length - 1));
             this.currentAction = this.actions10[next];
         }
         else if (random < 30) {
@@ -49,13 +59,13 @@ function Game (users, actions70, actions20, actions10) {
             next = Math.floor(Math.random() * (this.actions70.length - 1));
             this.currentAction = this.actions70[next];
         }
-        
+
         this.currentPlayer = (++this.currentPlayer) % (this.users.length);
-        
+
         if (typeof this.currentAction === Rule) {
             if (this.currentAction.withPlayer) {
                 this.currentAction.user = this.users[this.currentPlayer];
-                
+
             }
             else {
                 //TODO alte regeln löschen
